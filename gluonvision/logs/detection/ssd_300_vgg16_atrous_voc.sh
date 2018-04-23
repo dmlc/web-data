c35a2853a080f1948682982864f07a95af4e7dbb
Joshua Zhang <cheungchih@gmail.com>
2018-04-23 12:25:35 -0700

    bash

--- gluonvision/logs/detection/ssd_300_vgg16_atrous_voc.sh
+++ gluonvision/logs/detection/ssd_300_vgg16_atrous_voc.sh
@@ -1,2 +1,1 @@
-#!/usr/bin/env python
-train_ssd.py --gpus 0,1,2,3 -j 32 --network vgg16_atrous --data-shape 300 --dataset voc --lr 0.001 --lr-steps 160,200 --lr-decay 0.1 --epochs 240
+python train_ssd.py --gpus 0,1,2,3 -j 32 --network vgg16_atrous --data-shape 300 --dataset voc --lr 0.001 --lr-steps 160,200 --lr-decay 0.1 --epochs 240
--- gluonvision/logs/detection/ssd_512_resnet50_v1_voc.sh
+++ gluonvision/logs/detection/ssd_512_resnet50_v1_voc.sh
@@ -1,2 +1,1 @@
-#!/usr/bin/env python
-train_ssd.py --gpus 0,1,2,3 -j 32 --network resnet50_v1 --data-shape 512 --dataset voc --lr 0.001 --lr-steps 160,200 --lr-decay 0.1 --epochs 240
+python train_ssd.py --gpus 0,1,2,3 -j 32 --network resnet50_v1 --data-shape 512 --dataset voc --lr 0.001 --lr-steps 160,200 --lr-decay 0.1 --epochs 240
