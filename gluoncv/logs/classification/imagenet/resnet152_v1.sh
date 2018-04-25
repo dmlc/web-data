31a9e0cdf646f771b981ed139ffa592f110ee194
Tong He <hetong007@gmail.com>
2018-04-25 14:51:52 -0700

    add imagenet resnet training scripts

---
+++ gluoncv/logs/classification/imagenet/resnet152_v1.sh
@@ -0,0 +1,1 @@
+python train_imagenet.py --batch-size 64 --num-gpus 4 -j 32 --mode hybrid --num-epochs 150 --lr 0.1 --momentum 0.9 --wd 0.0001 --lr-decay 0.1 --lr-decay-epoch 30,60,90 --model resnet152_v1
