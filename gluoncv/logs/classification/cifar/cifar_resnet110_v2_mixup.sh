898b202efef803db74220816da865ea7ef08d829
Tong He <hetong007@gmail.com>
2018-05-07 01:31:38 -0700

    update cifar training logs

--- gluoncv/logs/classification/cifar/cifar_resnet110_v2_mixup.sh
+++ gluoncv/logs/classification/cifar/cifar_resnet110_v2_mixup.sh
@@ -1,1 +1,1 @@
-python train_mixup_cifar10.py --num-epochs 350 --mode hybrid --num-gpus 1 -j 8 --batch-size 128 --wd 0.0001 --lr 0.1 --lr-decay 0.1 --lr-decay-epoch 150,250 --model cifar_resnet110_v2
+python train_mixup_cifar10.py --num-epochs 220 --mode hybrid --num-gpus 1 -j 2 --batch-size 128 --wd 0.0001 --lr 0.1 --lr-decay 0.1 --lr-decay-epoch 100,150 --model cifar_resnet110_v2
--- gluoncv/logs/classification/cifar/cifar_wideresnet28_10_mixup.sh
+++ gluoncv/logs/classification/cifar/cifar_wideresnet28_10_mixup.sh
@@ -1,1 +1,1 @@
-python train_mixup_cifar10.py --num-epochs 350 --mode hybrid --num-gpus 1 -j 8 --batch-size 128 --wd 0.0001 --lr 0.1 --lr-decay 0.1 --lr-decay-epoch 80,160,240 --model cifar_wideresnet28_10
+python train_mixup_cifar10.py --num-epochs 220 --mode hybrid --num-gpus 2 -j 2 --batch-size 64 --wd 0.0005 --lr 0.1 --lr-decay 0.2 --lr-decay-epoch 60,120,160 --model cifar_wideresnet28_10
