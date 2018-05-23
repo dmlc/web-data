606d5c80e1551e83089d11d8f96722ecc22dfd30
Tong He <hetong007@gmail.com>
2018-05-23 11:21:32 -0700

    update cifar resnext training log

--- gluoncv/logs/classification/cifar/cifar_resnext29_16x64d.sh
+++ gluoncv/logs/classification/cifar/cifar_resnext29_16x64d.sh
@@ -1,1 +1,1 @@
-python train_cifar10.py --num-epochs 300 --mode hybrid --num-gpus 4 -j 4 --batch-size 32 --wd 0.0001 --lr 0.1 --lr-decay 0.1 --lr-decay-epoch 150,225 --model cifar_resnext29_16x64d
+python train_cifar10.py --num-epochs 300 --mode hybrid --num-gpus 4 -j 4 --batch-size 32 --wd 0.0005 --lr 0.1 --lr-decay 0.1 --lr-decay-epoch 150,225 --model cifar_resnext29_16x64d
