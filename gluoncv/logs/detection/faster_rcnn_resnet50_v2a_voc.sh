edab383831250b3c1c38f4fa8d4edbd0b094affe
Joshua Z. Zhang <cheungchih@gmail.com>
2018-06-18 13:06:37 -0700

    add training logs for detection models

---
+++ gluoncv/logs/detection/faster_rcnn_resnet50_v2a_voc.sh
@@ -0,0 +1,1 @@
+python3 train_faster_rcnn.py --gpus 0,1,2,3 --network resnet50_v2a -j 8 --dataset voc --lr 0.001 --lr-steps 14,20 --lr-decay 0.1 --epochs 30
---
+++ gluoncv/logs/detection/ssd_512_resnet50_v1_coco.sh
@@ -0,0 +1,1 @@
+python3 train_ssd.py --gpus 0,1,2,3 -j 32 --network resnet50_v1 --data-shape 512 --dataset coco --lr 0.001 --lr-steps 160,200 --lr-decay 0.1 --epochs 240
