f5546159113dc4ab3c91b7346dd806ba43f7c7f0
Tong He <hetong007@gmail.com>
2018-07-23 13:51:03 -0700

    add vgg model training logs

--- gluoncv/logs/classification/imagenet/vgg19_bn.sh
+++ gluoncv/logs/classification/imagenet/vgg19_bn.sh
@@ -1,1 +1,7 @@
-python train_imagenet.py --batch-size 32 --num-gpus 8 -j 64 --mode hybrid --num-epochs 100 --lr 0.01 --lr-decay 0.1 --lr-decay-epoch 50,80 --model vgg19_bn
+python train_imagenet.py \
+  --rec-train /media/ramdisk/rec/train.rec --rec-train-idx /media/ramdisk/rec/train.idx \
+  --rec-val /media/ramdisk/rec/val.rec --rec-val-idx /media/ramdisk/rec/val.idx \
+  --model vgg19_bn --mode hybrid --batch-norm \
+  --batch-size 32 --num-gpus 8 -j 64 \
+  --num-epochs 100 --lr 0.01 --lr-decay 0.1 --lr-decay-epoch 50,80 \
+  --use-rec --save-dir params_vgg19_bn
