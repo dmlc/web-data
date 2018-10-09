889ce8eb3d739a84dff84a550761a481f535e125
Tong He <hetong007@gmail.com>
2018-10-08 18:13:12 -0700

    rename mixup logs

--- gluoncv/logs/classification/imagenet/resnet152_v1d.sh
+++ gluoncv/logs/classification/imagenet/resnet152_v1d.sh
@@ -2,8 +2,7 @@ python train_imagenet.py \
   --rec-train /media/ramdisk/rec/train.rec --rec-train-idx /media/ramdisk/rec/train.idx \
   --rec-val /media/ramdisk/rec/val.rec --rec-val-idx /media/ramdisk/rec/val.idx \
   --model resnet152_v1d --mode hybrid \
-  --lr 0.2 --lr-mode cosine --num-epochs 120 --batch-size 64 --num-gpus 8 -j 60 \
-  --warmup-epochs 5 \
-  --use-rec --last-gamma --no-wd --label-smoothing \
-  --save-dir params_resnet152_v1d_best \
-  --logging-file resnet152_v1d_best.log
+  --lr 0.1 --lr-mode cosine --num-epochs 200 --batch-size 32 --num-gpus 8 -j 60 \
+  --use-rec --warmup-epochs 5 --last-gamma --no-wd --label-smoothing --mixup \
+  --save-dir params_resnet152_v1d_mixup \
+  --logging-file resnet152_v1d_mixup.log
--- gluoncv/logs/classification/imagenet/resnet50_v1d-mixup.sh
+++
@@ -1,8 +0,0 @@
-python train_imagenet.py \
-  --rec-train /media/ramdisk/rec/train.rec --rec-train-idx /media/ramdisk/rec/train.idx \
-  --rec-val /media/ramdisk/rec/val.rec --rec-val-idx /media/ramdisk/rec/val.idx \
-  --model resnet50_v1d --mode hybrid \
-  --lr 0.4 --lr-mode cosine --num-epochs 200 --batch-size 128 --num-gpus 8 -j 60 \
-  --use-rec --dtype float16 --warmup-epochs 5 --last-gamma --no-wd --label-smoothing --mixup \
-  --save-dir params_resnet50_v1d_mixup \
-  --logging-file resnet50_v1d_mixup.log
