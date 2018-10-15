c3dd9631e11fb2cd0eb1183ada7dbfc5d788a8cc
Tong He <hetong007@gmail.com>
2018-10-15 13:15:20 -0700

    update resnet152 logs

--- gluoncv/logs/classification/imagenet/resnet152_v1b.sh
+++ gluoncv/logs/classification/imagenet/resnet152_v1b.sh
@@ -2,6 +2,8 @@ python train_imagenet.py \
   --rec-train /media/ramdisk/rec/train.rec --rec-train-idx /media/ramdisk/rec/train.idx \
   --rec-val /media/ramdisk/rec/val.rec --rec-val-idx /media/ramdisk/rec/val.idx \
   --model resnet152_v1b --mode hybrid \
-  --lr 0.4 --lr-mode cosine --num-epochs 180 --batch-size 128 --num-gpus 8 -j 60 \
-  --warmup-epochs 5 --use-rec --dtype float16 --last-gamma \
-  --save-dir params_resnet152_v1b_180
+  --lr 0.1 --lr-mode cosine --num-epochs 120 --batch-size 32 --num-gpus 8 -j 60 \
+  --warmup-epochs 5 \
+  --use-rec --last-gamma --no-wd --label-smoothing \
+  --save-dir params_resnet152_v1b_best \
+  --logging-file resnet152_v1b_best.log
