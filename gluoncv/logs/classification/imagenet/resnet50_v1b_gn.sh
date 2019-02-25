6330a510b758bcea51b00ea2d24273dc885bd14e
Hang Zhang <8041160+zhanghang1989@users.noreply.github.com>
2019-02-25 15:01:56 -0800

    Update resnet50_v1b_gn.sh

--- gluoncv/logs/classification/imagenet/resnet50_v1b_gn.sh
+++ gluoncv/logs/classification/imagenet/resnet50_v1b_gn.sh
@@ -4,6 +4,6 @@ python train_imagenet.py \
   --model resnet50_v1b --mode hybrid \
   --lr 0.4 --lr-mode cosine --num-epochs 120 --batch-size 128 --num-gpus 8 -j 60 \
   --warmup-epochs 5 --dtype float32 \
-  --use-rec --last-gamma --no-wd --label-smoothing \
+  --use-rec --last-gamma --no-wd --label-smoothing  --use-gn \
   --save-dir params_resnet50_v1b_gn_best \
-  --logging-file resnet50_v1b_gn_best.log --use-gn
+  --logging-file resnet50_v1b_gn_best.log
