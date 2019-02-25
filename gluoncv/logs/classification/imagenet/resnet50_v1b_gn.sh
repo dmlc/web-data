255ed7228ab45e9c272523ba4dd089835863e980
Hang Zhang <8041160+zhanghang1989@users.noreply.github.com>
2019-02-25 14:43:04 -0800

    Add files via upload

---
+++ gluoncv/logs/classification/imagenet/resnet50_v1b_gn.sh
@@ -0,0 +1,9 @@
+python train_imagenet.py \
+  --rec-train /media/ramdisk/rec/train.rec --rec-train-idx /media/ramdisk/rec/train.idx \
+  --rec-val /media/ramdisk/rec/val.rec --rec-val-idx /media/ramdisk/rec/val.idx \
+  --model resnet50_v1b --mode hybrid \
+  --lr 0.4 --lr-mode cosine --num-epochs 120 --batch-size 128 --num-gpus 8 -j 60 \
+  --warmup-epochs 5 --dtype float32 \
+  --use-rec --last-gamma --no-wd --label-smoothing \
+  --save-dir params_resnet50_v1b_gn_best \
+  --logging-file resnet50_v1b_gn_best.log --use-gn
