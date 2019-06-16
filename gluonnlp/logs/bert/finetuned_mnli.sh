0c52f625651a84808c33fd0b53af3f402bc73a01
Haibin Lin <linhaibin.eric@gmail.com>
2019-06-15 22:23:13 -0400

    Add shell command for BERT experiments (#189)
    * Create finetune_squad1.1_base_mx1.5.0b20190216.sh
    
    * Create finetune_squad1.1_large_mx1.5.0b20190216.sh
    
    * Create finetuned_mnli.sh
    
    * Create finetuned_mrpc.sh
    
    * Create finetuned_rte.sh
    
    * Create finetuned_xnli.sh

---
+++ gluonnlp/logs/bert/finetuned_mnli.sh
@@ -0,0 +1,1 @@
+python finetune_classifier.py --task_name MNLI --max_len 80 --log_interval 100 --epsilon 1e-8 --gpu 0
---
+++ gluonnlp/logs/bert/finetuned_rte.sh
@@ -0,0 +1,1 @@
+python finetune_classifier.py --task_name RTE --batch_size 32 --epochs 3 --gpu 0 --lr 2e-5
