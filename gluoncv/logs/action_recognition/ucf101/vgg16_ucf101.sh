b5d077298ba543217550333b4099d24b52edea39
Yi Zhu <yizhu59@gmail.com>
2019-09-07 21:10:33 -0700

    Add TSN kinetics 400 (#202)
    * add action recognition training log for gluoncv
    
    * remove pretrained arg
    
    * add kinetics400

--- gluoncv/logs/action_recognition/ucf101/vgg16_ucf101.sh
+++ gluoncv/logs/action_recognition/ucf101/vgg16_ucf101.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 
 python ./scripts/action-recognition/train_recognizer.py \
-	--data-dir /home/ubuntu/yizhu/data/UCF101/rawframes \
-	--train-list /home/ubuntu/yizhu/data/UCF101/ucfTrainTestlist/ucf101_train_split_1_rawframes.txt \
-	--val-list /home/ubuntu/yizhu/data/UCF101/ucfTrainTestlist/ucf101_val_split_1_rawframes.txt \
+    --dataset ucf101 \
 	--model vgg16_ucf101 \
 	--num-classes 101 \
 	--mode hybrid \
+	--dtype float32 \
 	--batch-size 25 \
 	--num-gpus 8 \
 	--num-data-workers 32 \
@@ -22,5 +21,3 @@ python ./scripts/action-recognition/train_recognizer.py \
 	--num-epochs 80 \
 	--save-frequency 5 \
 	--log-interval 10 \
-	--logging-file 2d_rgb_vgg16_f1s1_b25_g8.txt \
-	--save-dir /home/ubuntu/yizhu/logs/mxnet/2d_rgb_vgg16_f1s1_b25_g8 \
--- gluoncv/logs/action_recognition/ucf101/vgg16_ucf101_tsn.sh
+++ gluoncv/logs/action_recognition/ucf101/vgg16_ucf101_tsn.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 
 python ./scripts/action-recognition/train_recognizer.py \
-	--data-dir /home/ubuntu/yizhu/data/UCF101/rawframes \
-	--train-list /home/ubuntu/yizhu/data/UCF101/ucfTrainTestlist/ucf101_train_split_1_rawframes.txt \
-	--val-list /home/ubuntu/yizhu/data/UCF101/ucfTrainTestlist/ucf101_val_split_1_rawframes.txt \
+	--dataset ucf101 \
 	--model vgg16_ucf101 \
 	--num-classes 101 \
 	--mode hybrid \
+	--dtype float32 \
 	--batch-size 25 \
 	--num-segments 3 \
 	--use-tsn \
@@ -24,5 +23,3 @@ python ./scripts/action-recognition/train_recognizer.py \
 	--num-epochs 80 \
 	--save-frequency 5 \
 	--log-interval 10 \
-	--logging-file tsn_2d_rgb_vgg16_seg_3_f1s1_b25_g8.txt \
-	--save-dir /home/ubuntu/yizhu/logs/mxnet/tsn_2d_rgb_vgg16_seg_3_f1s1_b25_g8 \
