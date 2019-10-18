7fd48bbcc8a8c3520c829b09a58e84ebf04da662
bryanzhu <yizhu59@gmail.com>
2019-10-18 10:28:01 -0700

    training bash and logs for nonlocal

---
+++ gluoncv/logs/action_recognition/kinetics400/i3d_nl5_resnet50_v1_kinetics400.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python ./scripts/action-recognition/train_recognizer.py \
+    --dataset kinetics400 \
+    --data-dir /home/ubuntu/third_disk/data/kinetics400/rawframes_train \
+    --val-data-dir /home/ubuntu/third_disk/data/kinetics400/rawframes_val \
+    --train-list /home/ubuntu/third_disk/data/kinetics400/kinetics400_train_list_rawframes_full.txt \
+    --val-list /home/ubuntu/third_disk/data/kinetics400/kinetics400_val_list_rawframes_clean.txt \
+    --dtype float32 \
+    --mode hybrid \
+    --prefetch-ratio 1.0 \
+    --input-5d \
+    --model i3d_nl5_resnet50_v1_kinetics400 \
+    --num-classes 400 \
+    --batch-size 16 \
+    --num-gpus 8 \
+    --num-data-workers 32 \
+    --input-size 224 \
+    --new-height 256 \
+    --new-width 340 \
+    --new-length 32 \
+    --new-step 2 \
+    --lr-mode step \
+    --lr-decay 0.1 \
+    --lr-decay-epoch 40,80,100 \
+    --lr 0.01 \
+    --momentum 0.9 \
+    --wd 0.0001 \
+    --num-epochs 100 \
+    --scale-ratios 1.0,0.8 \
+    --save-frequency 5 \
+    --clip-grad 40 \
+    --log-interval 20 \
+    --logging-file i3d_nl5_resnet50_v1_kinetics400.log \
+    --save-dir ./logs/ \
