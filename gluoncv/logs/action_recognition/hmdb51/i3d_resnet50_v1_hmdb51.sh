c297afdab8c8d0da6280c3b959e0e0d75b54004e
bryanzhu <yizhu59@gmail.com>
2019-10-30 15:01:10 -0700

    add bash and logs for ucf101 and hmdb51

---
+++ gluoncv/logs/action_recognition/hmdb51/i3d_resnet50_v1_hmdb51.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python ./scripts/action-recognition/train_recognizer.py \
+    --dataset hmdb51 \
+    --data-dir /home/ubuntu/.mxnet/datasets/hmdb51/rawframes \
+    --train-list /home/ubuntu/.mxnet/datasets/hmdb51/testTrainMulti_7030_splits/hmdb51_train_split_1_rawframes.txt \
+    --val-list /home/ubuntu/.mxnet/datasets/hmdb51/testTrainMulti_7030_splits/hmdb51_val_split_1_rawframes.txt \
+    --mode hybrid \
+    --dtype float32 \
+    --prefetch-ratio 1.0 \
+    --model i3d_resnet50_v1_hmdb51 \
+    --num-classes 51 \
+    --batch-size 8 \
+    --num-gpus 8 \
+    --num-data-workers 32 \
+    --input-size 224 \
+    --new-height 256 \
+    --new-width 340 \
+    --new-length 32 \
+    --new-step 2 \
+    --lr 0.01 \
+    --lr-decay 0.1 \
+    --lr-mode step \
+    --lr-decay-epoch 15,25,35 \
+    --momentum 0.9 \
+    --wd 0.0001 \
+    --num-epochs 35 \
+    --scale-ratios 1.0,0.8 \
+    --save-frequency 5 \
+    --clip-grad 40 \
+    --log-interval 20 \
+    --logging-file i3d_resnet50_v1_hmdb51.log \
+    --save-dir ./logs/
