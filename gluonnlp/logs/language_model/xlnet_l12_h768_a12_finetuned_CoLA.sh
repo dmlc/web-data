3b04f4a54c340d5dcd1a388b549a9af3aea6e75b
Wang <zechenw@a483e7462a21.ant.amazon.com>
2019-11-22 17:33:24 +0800

    add xlnet glue log&command

---
+++ gluonnlp/logs/language_model/xlnet_l12_h768_a12_finetuned_CoLA.sh
@@ -0,0 +1,2 @@
+python run_glue.py --gpu 8 --task_name CoLA --model_name xlnet_cased_l12_h768_a12 --lr 3e-5 --seed 77 --epochs 10 --batch_size 32 --dropout 0 --attention_dropout 0 --warmup_ratio 0.1
+
