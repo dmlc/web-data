c02ceef6ea338a04b059fbf6cfe8add052ee4b70
bryanzhu <yizhu59@gmail.com>
2019-11-25 21:52:47 -0800

    change nonlocal add slowfast

--- gluoncv/logs/action_recognition/kinetics400/slowfast_4x16_resnet50_kinetics400.sh
+++ gluoncv/logs/action_recognition/kinetics400/slowfast_4x16_resnet50_kinetics400.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
 
-CUDA_VISIBLE_DEVICES=7,6,5,4,3,2,1,0 python ./scripts/action-recognition/train_recognizer.py \
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python ./scripts/action-recognition/train_recognizer.py \
     --dataset kinetics400 \
     --data-dir /home/ubuntu/third_disk/data/kinetics400/rawframes_train \
     --val-data-dir /home/ubuntu/third_disk/data/kinetics400/rawframes_val \
     --train-list /home/ubuntu/third_disk/data/kinetics400/kinetics400_train_list_rawframes_full.txt \
     --val-list /home/ubuntu/third_disk/data/kinetics400/kinetics400_val_list_rawframes_full.txt \
-    --mode hybrid \
     --dtype float32 \
+    --mode hybrid \
     --prefetch-ratio 1.0 \
-    --model i3d_nl5_resnet101_v1_kinetics400 \
+    --model slowfast_4x16_resnet50_kinetics400 \
+    --slowfast \
+    --slow-temporal-stride 16 \
+    --fast-temporal-stride 2 \
     --num-classes 400 \
     --batch-size 8 \
     --num-gpus 8 \
@@ -17,17 +20,17 @@ CUDA_VISIBLE_DEVICES=7,6,5,4,3,2,1,0 python ./scripts/action-recognition/train_r
     --input-size 224 \
     --new-height 256 \
     --new-width 340 \
-    --new-length 32 \
-    --new-step 2 \
-    --lr-mode step \
-    --lr 0.01 \
+    --new-length 64 \
+    --new-step 1 \
+    --lr-mode cosine \
+    --lr 0.11 \
     --momentum 0.9 \
     --wd 0.0001 \
-    --lr-decay 0.1 \
-    --lr-decay-epoch 40,80,100 \
-    --num-epochs 100 \
+    --num-epochs 256 \
+    --warmup-epochs 37 \
+    --warmup-lr 0.001 \
     --scale-ratios 1.0,0.8 \
     --save-frequency 20 \
     --log-interval 50 \
-    --logging-file i3d_nl5_resnet101_v1_kinetics400.log \
+    --logging-file slowfast_4x16_resnet50_kinetics400.log \
     --save-dir ./logs/
