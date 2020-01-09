7535af2e353642ec55b36e939593d3e04a80ee8f
Wang <zechenw@a483e7462a21.ant.amazon.com>
2020-01-09 14:04:14 +0800

    add

---
+++ gluonnlp/logs/language_model/xlnet_l12_h768_a12_finetuned_QNLI.sh
@@ -0,0 +1,1 @@
+python run_glue.py --gpu 8 --seed 49 --task_name QNLI --model_name xlnet_cased_l12_h768_a12 --lr 4e-5
---
+++ gluonnlp/logs/language_model/xlnet_l12_h768_a12_finetuned_SST.sh
@@ -0,0 +1,1 @@
+python run_glue.py --gpu 8 --task_name SST --model_name xlnet_cased_l12_h768_a12 --lr 2e-5 --log_interval 50
