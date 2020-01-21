376a94580145fa60bc02b3d4624b943e67b565fa
zburning <798672141@qq.com>
2020-01-21 22:57:27 +0800

    [GluonNLP] More XLNet results using mxnet1.6.0rc1 (#228)
    * add xlnet squad results using mxnet1.6.0rc1
    
    * update xlnet mxnet1.6.0rc1 results

---
+++ gluonnlp/logs/language_model/xlnet_l12_h768_a12_finetuned_MRPC_mxnet1.6.0rc1.sh
@@ -0,0 +1,1 @@
+python3 run_glue.py --task_name MRPC --training_steps 800 --warmup_ratio 0.25 --seed 51 --batch_size 32 --gpu 2
---
+++ gluonnlp/logs/language_model/xlnet_l12_h768_a12_finetuned_RTE_mxnet1.6.0rc1.sh
@@ -0,0 +1,1 @@
+python3 run_glue.py --task_name RTE --gpu 2 --training_steps 800 --warmup_ratio 0.25 --seed 55 --batch_size 32
