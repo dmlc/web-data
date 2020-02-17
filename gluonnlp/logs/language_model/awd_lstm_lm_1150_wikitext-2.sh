867223334abae8c9b0a3c1b08d1990b86088c0fa
Ubuntu <ubuntu@ip-172-31-63-163.ec2.internal>
2020-02-17 04:42:33 +0000

    update language model commands

---
+++ gluonnlp/logs/language_model/awd_lstm_lm_1150_wikitext-2.sh
@@ -0,0 +1,1 @@
+python word_language_model.py --gpu 0 --tied --ntasgd --lr_update_interval 30 --lr_update_factor 0.1 --save awd_lstm_lm_1150_wikitext-2
---
+++ gluonnlp/logs/language_model/big_rnn_lm_2048_512_gbw-eval.sh
@@ -0,0 +1,1 @@
+python large_word_language_model.py --gpus 4 --eval-only --batch-size=1
