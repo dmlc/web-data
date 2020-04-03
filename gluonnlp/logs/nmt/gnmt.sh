dbed553fa8715b2db0efa19848ebcd15f85128d6
Chen Wang <zhanghuilian-98022@163.com>
2020-04-03 13:48:34 +0800

    Add shell scripts for nmt and sentiment

---
+++ gluonnlp/logs/nmt/gnmt.sh
@@ -0,0 +1,1 @@
+MXNET_GPU_MEM_POOL_TYPE=Round python train_gnmt.py --src_lang en --tgt_lang vi --batch_size 128 --optimizer adam --lr 0.001 --lr_update_factor 0.5 --beam_size 10 --bucket_scheme exp --num_hidden 512 --save_dir gnmt_en_vi_l2_h512_beam10 --epochs 12 --gpu 0
---
+++ gluonnlp/logs/nmt/transformer_en_de_u512.sh
@@ -0,0 +1,1 @@
+MXNET_GPU_MEM_POOL_TYPE=Round python train_transformer.py --dataset WMT2014BPE --src_lang en --tgt_lang de --batch_size 2700 --optimizer adam --num_accumulated 16 --lr 3.0 --warmup_steps 4000 --save_dir transformer_en_de_u512 --epochs 60 --gpus 0,1,2,3,4,5,6,7 --scaled  --average_start 15 --num_buckets 20 --bucket_scheme exp --bleu 13a —log_interval 10
