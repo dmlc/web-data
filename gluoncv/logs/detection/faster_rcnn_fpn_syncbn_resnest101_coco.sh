00b110b6ef571570c27d5bc11fc29f8fab2143f4
Jerry Zhang <jerryzh.cn@gmail.com>
2020-04-16 17:19:46 -0700

    ResNeSt Faster R-CNN training log and commands

---
+++ gluoncv/logs/detection/faster_rcnn_fpn_syncbn_resnest101_coco.sh
@@ -0,0 +1,1 @@
+python train_faster_rcnn.py --gpus 0,1,2,3,4,5,6,7 --dataset coco --batch-size 16 --use-fpn --lr 0.02 --lr-warmup 500 --rpn-smoothl1-rho 0.001 --rcnn-smoothl1-rho 0.001 -j16 --network resnest50 --executor-threads 8 --val-interval 1 --epochs 26 --lr-decay-epoch 20,24 --lr-warmup-factor 0.3333 --norm-layer syncbn --disable-hybridization
