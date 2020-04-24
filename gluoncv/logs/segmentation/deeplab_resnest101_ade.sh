9183b456f103ea721016e88451b03b7c15f485cc
Hang Zhang <zhang.hang@rutgers.edu>
2020-04-24 15:17:31 -0700

    Add ResNeSt + DeepLabV3 (#248)
    * Add files via upload
    
    * Create deeplab_resnest269_ade.sh
    
    * Create deeplab_resnest101_ade.sh
    
    * Create deeplab_resnest50_ade.sh

---
+++ gluoncv/logs/segmentation/deeplab_resnest101_ade.sh
@@ -0,0 +1,1 @@
+python train.py --dataset ade20k --model-zoo deeplab_resnest101_ade --aux --lr 0.01 --syncbn --ngpus 8 --checkname deeplab_resnest101_ade --epochs 120 --base-size 520 --crop-size 480 --workers 48
