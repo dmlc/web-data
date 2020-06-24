7a32b27abcb09a92d25ca21f10081feb5cdcd027
Xinyu Chen <xinyu1.chen@intel.com>
2020-06-24 10:34:50 +0800

    Create ssd_300_resnet34_v1b_coco.sh

---
+++ gluoncv/logs/detection/ssd_300_resnet34_v1b_coco.sh
@@ -0,0 +1,1 @@
+horovodrun -np 4 python3 train_ssd.py --gpus 0,1,2,3 -j 32 --network resnet34_v1b --data-shape 300 --dataset coco --lr 0.001 --lr-decay-epoch 160,200 --lr-decay 0.1 --epochs 240 --batch-size 64 --dali --horovod
