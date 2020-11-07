c1b73ca9b237040395474499cbb15a4c5543667a
bryanzhu <yizhu59@gmail.com>
2020-11-07 08:44:23 -0800

    change log

--- gluoncv/logs/segmentation/icnet_resnet50_mhpv1.sh
+++ gluoncv/logs/segmentation/icnet_resnet50_mhpv1.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python ./scripts/segmentation/train.py \
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15 python ./scripts/segmentation/train.py \
 	--dataset mhpv1 \
 	--model icnet \
 	--backbone resnet50 \
 	--syncbn \
-	--ngpus 8 \
+	--ngpus 16 \
 	--optimizer adam \
 	--lr 0.00001 \
 	--epochs 120 \
